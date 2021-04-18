64ac3088d8bf44b5feb05ae2da10b00ebd848b65
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-04-18 19:56:32 -0400

    try to fix install, no luck yet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="abraham3k",
-    version="1.1.2",
+    version="1.1.3",
     author="Calvin Kinateder",
     author_email="calvinkinateder@gmail.com",
     description="Algorithmically predict public sentiment on a topic using VADER sentiment analysis",
