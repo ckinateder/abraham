2cf857179d977d8e3d57a49c0637c4b798c68a02
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-04-20 12:00:30 -0400

    minor fix

--- abraham3k/prophets.py
+++ abraham3k/prophets.py
@@ -330,10 +330,9 @@ class Isaiah:
 
 
 if __name__ == "__main__":
-    print(f"Abraham Version: {open('version').read().strip()}")
+    # print(f"Abraham Version: {open('version').read().strip()}")
     darthvader = Isaiah(
         news_source="newsapi",
-        api_key="3530e04f04034a85a0ebf4d925c83c69",
         splitting=False,
     )  # splitting means that it recursively splits a large text into sentences and analyzes each individually
 
